6bfad5b6483aff2d75c3ea5ea171087a8d9a74bf
amomoal <adadashifuso@gmail.com>
2025-01-18 17:40:42 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.particles-js');
-    
-    // 粒子を生成
-    for (let i = 0; i < 50; i++) {
-        const particle = document.createElement('div');
-        particle.className = 'particle';
-        particle.style.top = `${Math.random() * 100}%`;
-        particle.style.left = `${Math.random() * 100}%`;
-        particle.style.animationDuration = `${Math.random() * 5 + 5}s`;
-        particle.style.animationDelay = `${Math.random() * 5}s`;
-        container.appendChild(particle);
-    }
-});
+
 
 // コピー機能を実装
 document.getElementById('copyButton').addEventListener('click', () => {
